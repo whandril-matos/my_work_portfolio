9b106b5d862b3497d6d5cc565e1879cbb58560d7
whandril-matos <whandrilmatos@gmail.com>
2024-01-28 22:18:15 -0300

    28012024

---
+++ logs/app/public/wp-content/plugins/optimole-wp/assets/build/dashboard/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives', 'wp-url'), 'version' => 'b883cc00e51abe385b05');
